1754465c31302ce67ed14e2c00dc631f9dcfacfd
Prathibha <pratibha.indel@gmail.com>
2022-05-31 17:38:46 -0400

    updated App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from './pages/Home'
+import Dashboard from './pages/Dashboard'
+import OnBoarding from './pages/OnBoarding'
+import Navbar from './component/Navbar'
+import { setContext } from "@apollo/client/link/context";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
 
-function App() {
+
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+//getting images through URL constructor which are not in public folder
+//const backGround=new URL("./images/picture2.png",import.meta.url)
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+
+const  App=()=> {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   
+    <div >
+      <Navbar/>
+    
+      <BrowserRouter>
+      <Routes>
+        <Route exact path={"/"} element ={<Home/>}/>
+        <Route path={"/dashboard"} element ={<Dashboard/>}/>
+        <Route path={"/onboarding"} element ={<OnBoarding/>}/>
+        <Route 
+            path='*'
+            element={<h1 className='display-2'>Wrong page!</h1>}
+          />
+            
+       
+      </Routes>
+      
+      </BrowserRouter>
     </div>
+    
   );
 }
 
