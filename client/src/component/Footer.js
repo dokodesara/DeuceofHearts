e8816d81fe53867ac60b4e7970a988213b47567b
Prathibha <pratibha.indel@gmail.com>
2022-06-03 13:29:23 -0400

    edited class with className

--- client/src/component/Footer.js
+++ client/src/component/Footer.js
@@ -20,12 +20,12 @@ const Footer = () => {
     <div style={style}>
 
 
-      <div class="ui inverted  grey vertical footer segment">
-        <div class="ui center aligned container">
-          <h4 class="ui inverted header">&copy; Copyright 2022 | All rights reserved </h4>
-          <a href="https://www.facebook.com/"><i class="facebook square icon big"></i></a>
-          <a href="https://twitter.com/"><i class="twitter square icon big"></i></a>
-          <a href="https://www.linkedin.com/company/c"><i class="linkedin square icon big"></i></a>
+      <div className="ui inverted  grey vertical footer segment">
+        <div className="ui center aligned container">
+          <h4 className="ui inverted header">&copy; Copyright 2022 | All rights reserved </h4>
+          <a href="https://www.facebook.com/"><i className="facebook square icon big"></i></a>
+          <a href="https://twitter.com/"><i className="twitter square icon big"></i></a>
+          <a href="https://www.linkedin.com/company/c"><i className="linkedin square icon big"></i></a>
         </div>
       </div>
     </div>
