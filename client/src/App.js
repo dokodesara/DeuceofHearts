7b844c8166dcb10fd4a3ea84b9d39d3ae7663244
Prathibha <pratibha.indel@gmail.com>
2022-06-02 23:19:58 -0400

    updated app.js with login and signup page,deleted restricted page,altered index.css

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,11 @@
+import React from 'react'
 import Home from './pages/Home'
 import Dashboard from './pages/Dashboard'
 import OnBoarding from './pages/OnBoarding'
-import Navbar from './component/Navbar'
+import Header from './component/Header'
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+
 import { setContext } from "@apollo/client/link/context";
 import {
   ApolloClient,
@@ -15,7 +19,7 @@ import { BrowserRouter,Routes,Route } from 'react-router-dom'
 //getting images through URL constructor which are not in public folder
 //const backGround=new URL("./images/picture2.png",import.meta.url)
 const httpLink = createHttpLink({
-  uri: "/graphql",
+  uri: "/graphql/",
 });
 
 const authLink = setContext((_, { headers }) => {
@@ -31,19 +35,24 @@ const authLink = setContext((_, { headers }) => {
 
 const client = new ApolloClient({
   link: authLink.concat(httpLink),
+  uri: "http://example.com/graphql/",
   cache: new InMemoryCache(),
 });
 
 
 const  App=()=> {
   return (
-   
-    <div >
-      <Navbar/>
+    <ApolloProvider client={client}>
+    
     
       <BrowserRouter>
+      <div>
+        <Header/>
+     
       <Routes>
         <Route exact path={"/"} element ={<Home/>}/>
+        <Route path={"/login"} element={<Login/>}/>
+        <Route path={"/Signup"} element={<Signup/>}/>
         <Route path={"/dashboard"} element ={<Dashboard/>}/>
         <Route path={"/onboarding"} element ={<OnBoarding/>}/>
         <Route 
@@ -53,10 +62,11 @@ const  App=()=> {
             
        
       </Routes>
+      </div>
       
       </BrowserRouter>
-    </div>
-    
+ 
+    </ApolloProvider>
   );
 }
 
